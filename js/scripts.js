0d3dd50ebcf57680e72bb305c50a676a037c13a6
vuduongtp <vuduongcalvin@gmail.com>
2023-02-09 20:58:42 +0700

    update cv 2023

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Resume v7.0.2 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
 //
 // Scripts
 // 
